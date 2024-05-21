384db5de58aa3b2bcabecb3dd8d25305a2aa46f9
tonymushah <tonyramanantsalama@gmail.com>
2024-05-21 07:22:09 +0300

    added `v_laptop` trigger

--- migrations/2024-05-21-013425_create_v_laptop/down.sql
+++ migrations/2024-05-21-013425_create_v_laptop/down.sql
@@ -1,1 +1,6 @@
 -- This file should undo anything in `up.sql`
+drop TRIGGER if exists V_LAPTOP_TRIGGER on V_LAPTOP;
+
+drop FUNCTION if exists V_LAPTOP_TRIGGER_FUNC;
+
+drop VIEW if exists V_LAPTOP;
--- migrations/2024-05-21-013425_create_v_laptop/up.sql
+++ migrations/2024-05-21-013425_create_v_laptop/up.sql
@@ -19,3 +19,53 @@ SELECT ID_LAPTOP,
 	LAPTOP.REF_LAPTOP AS REF_LAPTOP
 FROM LAPTOP
 JOIN REFERENCE_LAPTOP AS REF_LAP ON LAPTOP.REF_LAPTOP = REF_LAP.ID_REF;
+
+CREATE FUNCTION V_LAPTOP_TRIGGER_FUNC() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+BEGIN
+	If TG_OP = 'INSERT' THEN
+		Insert INTO
+			laptop(
+				id_laptop,
+				type_processeur,
+				carte_graphique,
+				ecran,
+				type_clavier,
+				prix_unitaire,
+				ref_laptop
+			)
+		values (
+			NEW.id_laptop,
+			NEW.type_processeur,
+			NEW.carte_graphique,
+			NEW.ecran,
+			NEW.type_clavier,
+			NEW.prix_unitaire,
+			NEW.ref_laptop
+		);
+		return NEW;
+	ELSEIF TG_OP = 'UPDATE' THEN
+		UPDATE laptop SET
+			type_processeur = NEW.type_processeur,
+			carte_graphique = NEW.carte_graphique,
+			ecran = NEW.ecran,
+			type_clavier = NEW.type_clavier,
+			prix_unitaire = NEW.prix_unitaire,
+			ref_laptop = NEW.ref_laptop
+		WHERE id_laptop = NEW.id_laptop;
+		return NEW;
+	ELSEIF TG_OP = 'DELETE' then
+		DELETE FROM laptop
+		where id_laptop = OLD.id_laptop;
+	END IF;
+	RETURN NULL;
+END;
+$$;
+
+
+CREATE TRIGGER V_LAPTOP_TRIGGER INSTEAD OF
+INSERT
+OR
+UPDATE
+OR
+DELETE ON V_LAPTOP
+FOR EACH ROW EXECUTE FUNCTION V_LAPTOP_TRIGGER_FUNC();
