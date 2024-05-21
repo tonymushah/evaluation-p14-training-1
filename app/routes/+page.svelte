255809faa4ca7d4c7b1cfe30537e18fc1d70109e
tonymushah <tonyramanantsalama@gmail.com>
2024-05-21 11:31:31 +0300

    Added shadcn

--- app/routes/+page.svelte
+++ app/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center bg-slate-200 w-screen h-screen">
+	<div class="bg-slate-400 rounded p-4 flex flex-col">
+		<h1 class="block font-bold text-2xl">Welcome to SvelteKit</h1>
+		<p>
+			Visit <a
+				class="text-red-600 hover:text-red-800 transition-colors"
+				href="https://kit.svelte.dev">kit.svelte.dev</a
+			> to read the documentation
+		</p>
+	</div>
+</div>
