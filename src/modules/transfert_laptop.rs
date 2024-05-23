4a200302b28d00c0bb013a150df53e5af0d0f90f
tonymushah <tonyramanantsalama@gmail.com>
2024-05-23 18:29:37 +0300

    Added `TransfertLaptopModule` `is_transferted_to` ,`transfert_to` and `get_stock_entry`

--- src/modules/transfert_laptop.rs
+++ src/modules/transfert_laptop.rs
@@ -1,12 +1,22 @@
 use async_graphql::ErrorExtensions;
+use diesel::prelude::*;
+use time::{OffsetDateTime, PrimitiveDateTime};
 use uuid::Uuid;
 
-use crate::DbPoolConnection;
+use crate::{
+    models::{
+        magasin_stock::MagasinStockEntry, sortie_stock::SortieStock,
+        transfert_en_cours::TransfertPendingEntry,
+    },
+    DbPoolConnection,
+};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("this laptop was already transfered to {0:?}")]
-    AlreadyTransfered(Option<Uuid>),
+    #[error("the given laptop was already transfered to {}", .0.to_string())]
+    AlreadyTransfered(Uuid),
+    #[error("the given laptop is not in stock")]
+    LaptopNotInStock,
 }
 
 impl ErrorExtensions for Error {
@@ -14,9 +24,10 @@ impl ErrorExtensions for Error {
         async_graphql::Error::new(format!("{self}")).extend_with(|_err, e| match self {
             Error::AlreadyTransfered(er) => {
                 e.set("code", "LAPTOP_ALREADY_TRANSFERED");
-                if let Some(lap) = er {
-                    e.set("to", lap.to_string());
-                }
+                e.set("to", er.to_string());
+            }
+            Error::LaptopNotInStock => {
+                e.set("code", "LAPTOP_NOT_IN_STOCK");
             }
         })
     }
@@ -27,4 +38,62 @@ pub struct TransfertLaptopModule<'a> {
     pub connection: &'a mut DbPoolConnection,
 }
 
-impl<'a> TransfertLaptopModule<'a> {}
+impl<'a> TransfertLaptopModule<'a> {
+    pub fn is_transfered_to(&mut self, point_vente_id: Uuid) -> crate::Result<Option<Uuid>> {
+        use crate::views::v_transfert_en_cours::dsl::*;
+        Ok(v_transfert_en_cours
+            .select(TransfertPendingEntry::as_select())
+            .filter(point_vente.eq(point_vente_id))
+            .limit(1)
+            .load(self.connection)?
+            .into_iter()
+            .map(|e: TransfertPendingEntry| e.point_vente)
+            .next())
+    }
+    pub fn get_stock_entry(&mut self) -> crate::Result<MagasinStockEntry> {
+        use crate::views::v_magasin_stock::dsl::*;
+        Ok(v_magasin_stock
+            .filter(laptop.eq(self.laptop_id))
+            .select(MagasinStockEntry::as_select())
+            .get_result(self.connection)?)
+    }
+    fn transfert<D: Into<PrimitiveDateTime>>(
+        &mut self,
+        point_vente_id: Uuid,
+        date: Option<D>,
+    ) -> crate::Result<SortieStock> {
+        let entry = self.get_stock_entry()?;
+        use crate::schema::sortie_stock::dsl::*;
+        let date: PrimitiveDateTime = date.map(|d| d.into()).unwrap_or_else(|| {
+            let now = OffsetDateTime::now_utc();
+            PrimitiveDateTime::new(now.date(), now.time())
+        });
+        let to_use = SortieStock {
+            id_sortie_stock: Uuid::new_v4(),
+            date_sortie: date,
+            point_vente: point_vente_id,
+            entree_stock: entry.id_entree_stock,
+        };
+        Ok(diesel::insert_into(sortie_stock)
+            .values(&to_use)
+            .returning(SortieStock::as_returning())
+            .get_result(self.connection)?)
+    }
+    pub fn transfert_to<D: Into<PrimitiveDateTime>>(
+        &mut self,
+        point_vente_id: Uuid,
+        date: Option<D>,
+    ) -> crate::Result<TransfertPendingEntry> {
+        if let Some(pv) = self.is_transfered_to(point_vente_id)? {
+            return Err(crate::Error::TransfertLaptopModule(
+                Error::AlreadyTransfered(pv),
+            ));
+        }
+        let sortie_entry = self.transfert(point_vente_id, date)?;
+        use crate::views::v_transfert_en_cours::dsl::*;
+        Ok(v_transfert_en_cours
+            .filter(id_demande.eq(sortie_entry.id_sortie_stock))
+            .select(TransfertPendingEntry::as_select())
+            .get_result(self.connection)?)
+    }
+}
