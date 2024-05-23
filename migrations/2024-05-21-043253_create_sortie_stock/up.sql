c5baf19d42c61d628d9c4eb92604dc19359facf7
tonymushah <tonyramanantsalama@gmail.com>
2024-05-23 16:16:19 +0300

    added `unique` constraint on `entree_stock` in `sortie_stock`

--- migrations/2024-05-21-043253_create_sortie_stock/up.sql
+++ migrations/2024-05-21-043253_create_sortie_stock/up.sql
@@ -2,5 +2,5 @@ create table sortie_stock(
     id_sortie_stock UUID PRIMARY KEY default gen_random_uuid(),
     date_sortie TIMESTAMP NOT NULL default now(),
     point_vente UUID NOT NULL References point_vente(id_point_vente),
-    entree_stock UUID NOT NULL References entree_stock(id_entree_stock)
+    entree_stock UUID NOT NULL UNIQUE References entree_stock(id_entree_stock)
 );
