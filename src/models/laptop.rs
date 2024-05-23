daaeaa37911a5fa5c941d16c91f11aa8302f9d64
tonymushah <tonyramanantsalama@gmail.com>
2024-05-23 15:11:35 +0300

    Added `LaptopInput` model

--- src/models/laptop.rs
+++ src/models/laptop.rs
@@ -1,8 +1,9 @@
+use async_graphql::InputObject;
 use bigdecimal::BigDecimal;
 use diesel::prelude::*;
 use uuid::Uuid;
 
-use crate::views::v_laptop;
+use crate::{schema::laptop, views::v_laptop};
 
 #[derive(
     Debug,
@@ -27,5 +28,32 @@ pub struct Laptop {
     pub ecran: BigDecimal,
     pub type_clavier: Uuid,
     pub prix_unitaire: BigDecimal,
+    pub ref_laptop: Uuid,
+}
+
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Identifiable,
+    Selectable,
+    Insertable,
+    Queryable,
+    AsChangeset,
+    InputObject,
+)]
+#[diesel(table_name = laptop)]
+#[diesel(primary_key(id_laptop))]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct LaptopInput {
+    pub id_laptop: Uuid,
+    pub type_processeur: Option<Uuid>,
+    pub carte_graphique: Option<Uuid>,
+    pub ecran: Option<BigDecimal>,
+    pub type_clavier: Option<Uuid>,
+    pub prix_unitaire: Option<BigDecimal>,
     pub ref_laptop: Uuid,
 }
