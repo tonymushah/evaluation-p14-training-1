7894ecab8f31392aafacbf6a03b1e671d6ecc269
tonymushah <tonyramanantsalama@gmail.com>
2024-05-23 09:02:47 +0300

    Fixed `create_sortie_stock`

--- migrations/2024-05-21-043253_create_sortie_stock/up.sql
+++ migrations/2024-05-21-043253_create_sortie_stock/up.sql
@@ -1,5 +1,6 @@
 create table sortie_stock(
     id_sortie_stock UUID PRIMARY KEY default gen_random_uuid(),
     date_sortie TIMESTAMP NOT NULL default now(),
-    point_vente UUID NOT NULL References point_vente(id_point_vente)
+    point_vente UUID NOT NULL References point_vente(id_point_vente),
+    entree_stock UUID NOT NULL References entree_stock(id_entree_stock)
 );
