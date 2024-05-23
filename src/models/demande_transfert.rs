6db36cea845513709ad69c3101f3d020bf03ffcb
tonymushah <tonyramanantsalama@gmail.com>
2024-05-23 15:55:29 +0300

    `MagasinStockEntry` ->  `TransfertDemandEntry` in `demande_transfert.rs`

--- src/models/demande_transfert.rs
+++ src/models/demande_transfert.rs
@@ -11,7 +11,7 @@ use crate::views::v_demande_transfert;
 #[diesel(table_name = v_demande_transfert)]
 #[diesel(primary_key(id_demande, entree_magasin, laptop))]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct MagasinStockEntry {
+pub struct TransfertDemandEntry {
     pub id_demande: Uuid,
     pub date_demande: PrimitiveDateTime,
     pub point_vente: Uuid,
