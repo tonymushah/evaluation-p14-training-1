3e11a274e33c962e0654834fa9146826f8d2acd9
tonymushah <tonyramanantsalama@gmail.com>
2024-05-24 11:08:04 +0300

    Complexified `ReferenceLaptop`

--- src/models/reference_laptop.rs
+++ src/models/reference_laptop.rs
@@ -1,9 +1,14 @@
-use async_graphql::{InputObject, SimpleObject};
+use async_graphql::{ComplexObject, Context, InputObject, SimpleObject};
 use bigdecimal::BigDecimal;
 use diesel::prelude::*;
 use uuid::Uuid;
 
-use crate::schema::reference_laptop;
+use crate::{graphql::GetPoolConnection, schema::reference_laptop};
+
+use super::{
+    type_carte_graphique::TypeCarteGraphique, type_clavier::TypeClavier,
+    type_processeur::TypeProcesseur,
+};
 
 #[derive(
     Debug,
@@ -20,16 +25,56 @@ use crate::schema::reference_laptop;
     SimpleObject,
     InputObject,
 )]
-#[graphql(input_name = "ReferenceLaptopInput")]
+#[graphql(input_name = "ReferenceLaptopInput", complex)]
 #[diesel(table_name = reference_laptop)]
 #[diesel(primary_key(id_ref))]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct ReferenceLaptop {
     #[graphql(default_with = "Uuid::new_v4()")]
     pub id_ref: Uuid,
+    #[graphql(skip_output)]
     pub type_processeur: Uuid,
+    #[graphql(skip_output)]
     pub carte_graphique: Uuid,
     pub ecran: BigDecimal,
+    #[graphql(skip_output)]
     pub type_clavier: Uuid,
     pub prix_unitaire: BigDecimal,
+}
+
+#[ComplexObject]
+impl ReferenceLaptop {
+    pub async fn processeur(&self, ctx: &Context<'_>) -> crate::Result<TypeProcesseur> {
+        let proc = self.type_processeur;
+        ctx.use_pool(move |mut pool| {
+            use crate::schema::type_processeur::dsl::*;
+            Ok(type_processeur
+                .filter(id_type_processeur.eq(proc))
+                .select(TypeProcesseur::as_select())
+                .get_result(&mut pool)?)
+        })
+        .await
+    }
+    pub async fn carte_graphique(&self, ctx: &Context<'_>) -> crate::Result<TypeCarteGraphique> {
+        let gpu = self.carte_graphique;
+        ctx.use_pool(move |mut pool| {
+            use crate::schema::type_carte_graphique::dsl::*;
+            Ok(type_carte_graphique
+                .filter(id_type.eq(gpu))
+                .select(TypeCarteGraphique::as_select())
+                .get_result(&mut pool)?)
+        })
+        .await
+    }
+    pub async fn clavier(&self, ctx: &Context<'_>) -> crate::Result<TypeClavier> {
+        let id = self.type_clavier;
+        ctx.use_pool(move |mut pool| {
+            use crate::schema::type_clavier::dsl::*;
+            Ok(type_clavier
+                .filter(id_type_clavier.eq(id))
+                .select(TypeClavier::as_select())
+                .get_result(&mut pool)?)
+        })
+        .await
+    }
 }
